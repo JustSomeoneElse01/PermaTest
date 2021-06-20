9f5e2998cf4f0a9d164988900661fb009a77f504
JustSomeoneElse01 <dpichemoore@gmail.com>
2021-06-20 13:31:28 -0600

    added print method

--- PermaTest/src/MainTest.java
+++ PermaTest/src/MainTest.java
@@ -1,21 +1,29 @@
-class test  {
+class Test  {
 		
 	public void message() {
 		System.out.println("test");
 	}
+	
+	public void printLine(String s) {
+		System.out.println(s);
+	}
+	
 }
+
 
 public class MainTest {
 	
 	public static void main(String[] args) {
 		
-		test main = new test();
+		Test test = new Test();
 		
 		String var = "Hello World!";
 		
 		System.out.println(var);
 		
-		main.message();
+		test.message();
+		
+		test.printLine("Hello there");
 		
 	}
 	
