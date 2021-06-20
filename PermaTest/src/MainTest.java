ade2e33ae3245c35a857fe23ad183e6fcee46657
JustSomeoneElse01 <dpichemoore@gmail.com>
2021-06-20 00:20:04 -0600

    added a message class

--- PermaTest/src/MainTest.java
+++ PermaTest/src/MainTest.java
@@ -1,14 +1,21 @@
-class hello1  {
+class test  {
 		
+	public void message() {
+		System.out.println("test");
+	}
 }
 
 public class MainTest {
 	
 	public static void main(String[] args) {
 		
+		test main = new test();
+		
 		String var = "Hello World!";
 		
 		System.out.println(var);
+		
+		main.message();
 		
 	}
 	
